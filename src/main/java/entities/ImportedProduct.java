3682a63cb1ff2fed8c182d0ca3bba932e2b2d4b7
Perkles <walisoncastor@gmail.com>
2023-12-14 14:55:36 -0300

    Exercicio Fixaxao: First commit

---
+++ src/main/java/entities/ImportedProduct.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class ImportedProduct extends Product{
+
+    private Double customsFee;
+
+    public ImportedProduct(){
+        super();
+    }
+
+    public ImportedProduct(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    public ImportedProduct(String name, Double price, Double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public Double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    @Override
+    public String priceTag() {
+        return super.getName();
+    }
+
+    public double totalPrice(){
+        return super.getPrice() + customsFee;
+    }
+}
