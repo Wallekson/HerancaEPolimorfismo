24c4cc05b3e454963dc93fba1a104e2d3d698e8e
Perkles <walisoncastor@gmail.com>
2023-12-18 15:37:37 -0300

    Exercicio Fixaxao: Classes

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import entities.ImportedProduct;
 import entities.Product;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -14,10 +16,35 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy");
+
         List<Product> list = new ArrayList<>();
 
         System.out.print("Enter the number of products: ");
         int n = sc.nextInt();
+
+
+        for (int i = 1; i < n; i++){
+            System.out.printf("Product #" + i + " data");
+            System.out.print("Common, used or imported (c/u/i)?");
+            char ch = sc.next().charAt(0);
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+            if (ch == 'i'){
+                System.out.print("Customs fee: ");
+                double customsFee = sc.nextDouble();
+                price += customsFee;
+                list.add(new ImportedProduct(name,price,customsFee));
+            } else
+                if (ch == 'u') {
+                    System.out.print("Manufacture date (DD/MM/YYYY: ");
+
+            }
+
+
+        }
 
 
 
