b5df914a8abad4262adbbc6f3bcec1f865e12114
Perkles <walisoncastor@gmail.com>
2023-12-28 13:56:14 -0300

    Exercicio Fixaxao: Date

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,12 +2,10 @@ package application;
 
 import entities.ImportedProduct;
 import entities.Product;
+import entities.UsedProduct;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Program {
 
@@ -16,7 +14,7 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         List<Product> list = new ArrayList<>();
 
@@ -28,18 +26,27 @@ public class Program {
             System.out.printf("Product #" + i + " data");
             System.out.print("Common, used or imported (c/u/i)?");
             char ch = sc.next().charAt(0);
-            System.out.print("Name: ");
-            String name = sc.nextLine();
-            System.out.print("Price: ");
-            double price = sc.nextDouble();
             if (ch == 'i'){
+                System.out.print("Name: ");
+                String name = sc.nextLine();
+                sc.nextLine();
+                System.out.print("Price: ");
+                Double price = sc.nextDouble();
                 System.out.print("Customs fee: ");
                 double customsFee = sc.nextDouble();
                 price += customsFee;
                 list.add(new ImportedProduct(name,price,customsFee));
             } else
                 if (ch == 'u') {
+                    System.out.print("Name: ");
+                    String name = sc.nextLine();
+                    sc.nextLine();
+                    System.out.print("Price: ");
+                    Double price = sc.nextDouble();
                     System.out.print("Manufacture date (DD/MM/YYYY: ");
+                    String manufacturedDate = sc.nextLine();
+                    list.add(new UsedProduct(name,price, sdf.format(manufacturedDate)));
+
 
             }
 
