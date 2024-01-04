bc6d161a54bd882000661d489efaab7bb6da0711
Perkles <walisoncastor@gmail.com>
2024-01-04 15:17:58 -0300

    First commit

---
+++ Exercicio-Tax-ClassesAbstratas/src/main/java/entities/Company.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class Company extends Tax {
+
+    private Integer numberEmployees;
+
+    public Company(){
+        super();
+    }
+
+    public Company(String name, Double annualIncome, Integer numberEmployees) {
+        super(name, annualIncome);
+        this.numberEmployees = numberEmployees;
+    }
+
+    public Integer getNumberEmployees() {
+        return numberEmployees;
+    }
+
+    public void setNumberEmployees(Integer numberEmployees) {
+        this.numberEmployees = numberEmployees;
+    }
+
+    @Override
+    public double tax(){
+        return numberEmployees;
+    }
+}
